b42f5a598eef68e2e808512a334f0d0d5b1befa4
luopengfei <504049354@qq.com>
2019-01-31 11:37:07 +0800

    ribbon 新增hytrix熔断机制

--- server-ribbon/src/main/java/com/serverribbon/serverribbon/service/TestService.java
+++ server-ribbon/src/main/java/com/serverribbon/serverribbon/service/TestService.java
@@ -1,5 +1,7 @@
 package com.serverribbon.serverribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +19,14 @@ public class TestService {
     @Autowired
     private RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod = "error")
     public String hi(String name){
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+    public String error(String name){
+
+        return name+"  sorry error!";
     }
 }
